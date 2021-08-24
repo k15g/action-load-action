cbabb78d892ace52ec4a6c03404c0effbae41d88
Erlend Klakegg Bergheim <erlend@klakegg.net>
2021-08-24 21:31:23 +0200

    Adding README

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -4,27 +4,36 @@ import * as fs from 'fs'
 import * as tar from 'tar'
 
 async function run() {
+    // Fetch inputs
     var octokit = github.getOctokit(core.getInput('token'))
     var actions = core.getInput('action', { required: true })
 
+    // Regex used to match action identifiers
     var regex = /^([\w\-]+)\/([\w\-]+)(@([\w\-]+))?$/
 
+    // Make sure the actions folder exists
     if (!fs.existsSync(`.github/actions`))
-        fs.mkdirSync(`.github/actions`)
+        fs.mkdirSync(`.github/actions`, { recursive: true })
 
     actions.split("\n").forEach(async action => {
         if (action.match(regex)) {
+            // Extract values from action identifier
             var [_, owner, repo, _, ref] = action.match(regex)
 
+            // Fetch action tar file
             var response = await octokit.rest.repos.downloadTarballArchive({ owner, repo, ref })
 
+            // Temporary save tar file to disk
             fs.writeFileSync(`.github/actions/${owner}-${repo}.tar.gz`, Buffer.from(response.data as ArrayBuffer))
 
+            // Remove existing version of action if available
             if (fs.existsSync(`.github/actions/${owner}/${repo}`))
                 fs.rmSync(`.github/actions/${owner}/${repo}`, { recursive: true })
 
+            // Create folder to extract action into
             fs.mkdirSync(`.github/actions/${owner}/${repo}`, { recursive: true })
 
+            // Extract action
             tar.extract({
                 file: `.github/actions/${owner}-${repo}.tar.gz`,
                 strip: 1,
@@ -32,8 +41,10 @@ async function run() {
                 sync: true
             })
 
+            // Remove temporary file
             fs.rmSync(`.github/actions/${owner}-${repo}.tar.gz`)
 
+            // Override .gitignore in downloaded action to "hide" the action in case of commits
             fs.writeFileSync(`.github/actions/${owner}/${repo}/.gitignore`, '*')
         } else {
             core.error(`Identifier '${action}' is not valid`)
